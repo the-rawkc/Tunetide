9f757def91da23ce1c95dcdcacacfe914b51d3f8
the-rawkc <ravirohela7017@gmail.com>
2024-07-20 23:08:28 +0530

    second commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { nextSong, prevSong, playPause } from '../../redux/features/playerSlice';
+import { nextSong, prevSong, playPause } from 'D:\Development\kuchbhiwebd\TuneTide2-\src\redux\features\playerSlice.js';
 import Controls from './Controls';
 import Player from './Player';
 import Seekbar from './Seekbar';
